efef1967a6831df6f9c0ad0abf07794273798265
kebir20 <abdelkebirchouiy@gmail.com>
2026-04-15 21:16:14 +0200

    Refactor loadTrending function to use static albums

--- music/music.js
+++ music/music.js
@@ -1,39 +1,40 @@
-async function loadTrending(){
+const container = document.getElementById("albums");
 
-const token = await getToken()
-
-const res = await fetch(
-"https://api.spotify.com/v1/browse/new-releases?limit=30",
+const albums = [
 {
-headers:{Authorization:"Bearer "+token}
-})
-
-const data = await res.json()
-
-const container=document.getElementById("albums")
-
-data.albums.items.forEach(album=>{
-
-container.innerHTML+=`
-
-<div class="card" onclick="openAlbum('${album.id}')">
-
-<img src="${album.images[0].url}">
-
-<p>${album.name}</p>
-
-</div>
-
-`
-
-})
-
+title:"After Hours",
+artist:"The Weeknd",
+cover:"https://i.scdn.co/image/ab67616d0000b273c6d1a8a4a7c4fa2f2b5756fa"
+},
+{
+title:"Starboy",
+artist:"The Weeknd",
+cover:"https://i.scdn.co/image/ab67616d0000b2734718e2b124f4c2e64e5b1e0d"
+},
+{
+title:"Divide",
+artist:"Ed Sheeran",
+cover:"https://i.scdn.co/image/ab67616d0000b2731e2e4f4b2b9d8e5f9f3f4042"
+},
+{
+title:"Justice",
+artist:"Justin Bieber",
+cover:"https://i.scdn.co/image/ab67616d0000b273f46b9d20259bd6e5a9f4c0c4"
 }
+];
 
-function openAlbum(id){
+albums.forEach(album => {
 
-window.location.href="album.html?id="+id
+const card = document.createElement("div");
 
-}
+card.style.width="160px";
+card.style.margin="10px";
 
-loadTrending()
+card.innerHTML = `
+<img src="${album.cover}" style="width:160px;border-radius:10px">
+<p>${album.title}</p>
+`;
+
+container.appendChild(card);
+
+});
