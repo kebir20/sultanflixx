913eb3603d544c91ee39ad736b9bc66beef1ad5b
kebir20 <abdelkebirchouiy@gmail.com>
2026-04-15 23:44:56 +0200

    Refactor loadTrending function to fetch new releases

--- music/music.js
+++ music/music.js
@@ -1,40 +1,33 @@
-const container = document.getElementById("albums");
+async function loadTrending(){
 
-const albums = [
+const token = await getToken()
+
+const res = await fetch(
+"https://api.spotify.com/v1/browse/new-releases?limit=20",
 {
-title:"After Hours",
-artist:"The Weeknd",
-cover:"https://i.scdn.co/image/ab67616d0000b273c6d1a8a4a7c4fa2f2b5756fa"
-},
-{
-title:"Starboy",
-artist:"The Weeknd",
-cover:"https://i.scdn.co/image/ab67616d0000b2734718e2b124f4c2e64e5b1e0d"
-},
-{
-title:"Divide",
-artist:"Ed Sheeran",
-cover:"https://i.scdn.co/image/ab67616d0000b2731e2e4f4b2b9d8e5f9f3f4042"
-},
-{
-title:"Justice",
-artist:"Justin Bieber",
-cover:"https://i.scdn.co/image/ab67616d0000b273f46b9d20259bd6e5a9f4c0c4"
+headers:{
+Authorization:"Bearer "+token
 }
-];
+})
 
-albums.forEach(album => {
+const data = await res.json()
 
-const card = document.createElement("div");
+const container=document.getElementById("albums")
 
-card.style.width="160px";
-card.style.margin="10px";
+data.albums.items.forEach(album=>{
 
-card.innerHTML = `
-<img src="${album.cover}" style="width:160px;border-radius:10px">
-<p>${album.title}</p>
-`;
+const card=document.createElement("div")
+card.className="card"
 
-container.appendChild(card);
+card.innerHTML=`
+<img src="${album.images[0].url}">
+<p>${album.name}</p>
+`
 
-});
+container.appendChild(card)
+
+})
+
+}
+
+loadTrending()
